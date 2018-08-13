77f27a12cd92ce60409a3f6095400798698eda43
EC2 Default User <ec2-user@ip-172-31-36-22.us-east-2.compute.internal>
2018-08-13 07:53:28 +0000

    revamped website - new build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css'
+import './Home.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
 //import Navbar from '../components/Navbar.jsx';
 //import Footer from '../components/Footer.jsx';
 
@@ -10,10 +13,12 @@ class Home extends Component {
             <div>
                 <div className="jumbotron text-center bg-dark">
 					<h1>Your programming solutions, here.</h1> 
-					<p>Customized scripts to automate boring tasks, fully functional websites built from the ground up, and more. </p> 
+					<p>Tech consulting and scripting solutions for tedious daily/business related tasks. Easy to use, and easier to place an order.</p> 
 					<p><Link className="btn btn-primary btn-lg" to="/order" role="button">Place an Order &raquo;</Link></p>
 				</div>
 				<Description />
+				<Timeline />
+				<ServiceList />
 				<MainPoints />
 				<Quote />
             </div>
@@ -26,33 +31,35 @@ class Description extends Component {
         return (
             <div className="bg-light">
                 <div className="container-fluid">
-                	<h1 className="text-center display-5" id="service">Our Services</h1>
-                	<br/>
                 	<div className="row">
                 		<div className="col-sm-1"/>
-	                	<div className="col-sm-5 text-center">
-	                		<span className="fab fa-python fa-10x"/>
-	                		<h2 className="display-6">Custom Scripts</h2>
-	                		<p className="lead">Automated solutions for tedious tasks.</p>
-	                		<p className="small">Send us specifications on the task that needs to be automated, and we will provide a script that will automate it, thus saving precious time that you could spend doing something else.</p>
-	                		<ul class="list-group">
-								<li class="list-group-item">Personal/Business Use</li>
-								<li class="list-group-item">Usually takes 1~2 days (depending on project)</li>
-								<li class="list-group-item">Email Support</li>
-							</ul>
-	                		<p><Link className="btn btn-primary btn-lg" to="/scripts" role="button">Learn More &raquo;</Link></p>
+	                	<div className="col-sm-2 text-center">
+	                		<span className="fas fa-clipboard-list fa-10x"/>
 	                	</div>
-	                	<div className="col-sm-5 text-center">
-	                		<span className="fab fa-html5 fa-10x"/>
-	                		<h2 className="display-6">Personalized Website</h2>
-	                		<p className="lead">For yourself, for your company, for your pets.</p>
-	                		<p className="small">We will build your website for you. Full support from setting up a server/domain to maintenance post-development. We will use wordpress or build it from the ground up on request.</p>
-	                		<ul class="list-group">
-								<li class="list-group-item">Personal/Business Use</li>
-								<li class="list-group-item">Can take more than a week (depending on project)</li>
-								<li class="list-group-item">Full Email/Chat Support (on any SMS platform)</li>
-							</ul>
-	                		<p><Link className="btn btn-primary btn-lg" to="/webdev" role="button">Learn More &raquo;</Link></p>
+	                	<div className="col-sm-8 text-center">
+	                		<h2 className="display-6">Small Scale Tech Consulting</h2>
+	                		<h4 className="display-6">We will help you evaluate any tedious or inefficient work in your business and present automated solutions.</h4>
+	                		<p>Any business has their fair share of tedious tasks. Filling out excel sheets, gathering information from online sources, keeping track of emails, and so on. 
+	                		You can submit these tasks to us, and we can help you create an automated program that can greatly reduce the amount of time spent on these tasks.
+	                		Since everything will be done remotely, communication will be through skype or your messaging platform of choice. The solution will be well described and documented
+	                		to prevent any kind of miscommunication in order to make sure that the program fits the job.
+	                		</p>
+	                	</div>
+	                	<div className="col-sm-1"/>
+	                </div>
+	                <div className="row">
+                		<div className="col-sm-1"/>
+	                	<div className="col-sm-8 text-center">
+	                		<h2 className="display-6">Process Optimization</h2>
+	                		<h4 className="display-6">Optimize your processes.</h4>
+	                		<p>Any business has their fair share of tedious tasks. Filling out excel sheets, gathering information from online sources, keeping track of emails, and so on. 
+	                		You can submit these tasks to us, and we can help you create an automated program that can greatly reduce the amount of time spent on these tasks.
+	                		Since everything will be done remotely, communication will be through skype or your messaging platform of choice. The solution will be well described and documented
+	                		to prevent any kind of miscommunication in order to make sure that the program fits the job.
+	                		</p>
+	                	</div>
+	                	<div className="col-sm-2 text-center">
+	                		<span className="fas fa-code fa-10x"/>
 	                	</div>
 	                	<div className="col-sm-1"/>
 	                </div>
@@ -60,6 +67,80 @@ class Description extends Component {
             </div>
         );
     }
+}
+
+class Timeline extends Component {
+	render() {
+		return (
+			<div className="bg-dark container-fluid">
+				<h1 className="text-center display-5">General Timeline</h1><br/>
+				<VerticalTimeline>
+					  <VerticalTimelineElement
+					    className="vertical-timeline-element--work"
+					    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+					  >
+					    <h3 className="vertical-timeline-element-title">Consulting</h3>
+					    <h4 className="vertical-timeline-element-subtitle">1~2 days</h4>
+					    <p>
+					      Discuss time consuming business processes and potential programmable solutions.
+					    </p>
+					  </VerticalTimelineElement>
+					  <VerticalTimelineElement
+					    className="vertical-timeline-element--work"
+					    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+					  >
+					    <h3 className="vertical-timeline-element-title">Preparation</h3>
+					    <h4 className="vertical-timeline-element-subtitle">Same Day</h4>
+					    <p>
+					      Plan out timeline, from development of tool to deployment and even training. Gather required resources for testing (explained below).
+					    </p>
+					  </VerticalTimelineElement>
+					  <VerticalTimelineElement
+					    className="vertical-timeline-element--work"
+					    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+					  >
+					    <h3 className="vertical-timeline-element-title">Development</h3>
+					    <h4 className="vertical-timeline-element-subtitle">1~ days (depends on scale)</h4>
+					    <p>
+					      Tool will be developed and tested according to parameters given by customer. Program will be tested and documentation will be created for ease of use.
+					    </p>
+					  </VerticalTimelineElement>
+					  <VerticalTimelineElement
+					    className="vertical-timeline-element--work"
+					    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+					  >
+					    <h3 className="vertical-timeline-element-title">Deployment</h3>
+					    <h4 className="vertical-timeline-element-subtitle">1 day</h4>
+					    <p>
+					      Deliver tool to customer, and provide assistance until the script is working properly.
+					    </p>
+					  </VerticalTimelineElement>
+					  <VerticalTimelineElement
+					    className="vertical-timeline-element--education"
+					    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+					  >
+					    <h3 className="vertical-timeline-element-title">Technical Support</h3>
+					    <h4 className="vertical-timeline-element-subtitle">To infinity, and beyond.</h4>
+					    <p>
+					      24/7 assistance for any tools developed by us.
+					    </p>
+					  </VerticalTimelineElement>
+					  <VerticalTimelineElement
+					    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+					  />
+				</VerticalTimeline>	
+			</div>
+		);
+	}
+}
+
+class ServiceList extends Component {
+	render() {
+		return(
+			<div>
+			</div>
+		);
+	}
 }
 
 class MainPoints extends Component {
